3babbb7a3fd5af394121dd1a8efe6523fcd091e9
Wilian Ventura <61019210+besome@users.noreply.github.com>
2025-04-07 22:31:17 +0000

    implementação iniical do buscador de lanchonetes

--- src/app/modules/lanchonetes/lanchonetes.component.ts
+++ src/app/modules/lanchonetes/lanchonetes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-lanchonetes',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './lanchonetes.component.html',
   styleUrl: './lanchonetes.component.css'
 })
-export class LanchonetesComponent {
 
+export class LanchonetesComponent {
+  searchTerm : string = '';
+  search(e: Event) : void{
+   
+  const target = e.target as HTMLInputElement;
+  const value = target.value;
+    
+  }
 }
