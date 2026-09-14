4d07c836ddab94c532c140fdc562518303d934bd
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2026-09-14 18:46:49 -0300

    update packages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,35 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/pages/home/home.component';
-import { LanchonetesComponent } from './modules/lanchonetes/lanchonetes.component';
-import { CompararPrecosComponent } from './modules/comparar-precos/comparar-precos.component';
-import { Error404Component } from './modules/error/pages/error-404/error-404.component';
-import { LanchonetesInfoComponent } from './modules/lanchonetes-info/lanchonetes-info.component';
-import { ContatoComponent } from './modules/contato/contato.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, data: { hideFooter: true } },
-  { path: 'lanchonetes', component: LanchonetesComponent},
-  { path: 'lanchonete/:nome', component: LanchonetesInfoComponent },
-  { path: 'comparar-precos', component: CompararPrecosComponent},
-  { path: 'contato', component: ContatoComponent},
-  { path: '**', component: Error404Component }
+  {
+    path: '',
+    loadComponent: () =>
+      import('./modules/home/pages/home/home.component').then((m) => m.HomeComponent),
+    data: { hideFooter: true }
+  },
+  {
+    path: 'lanchonetes',
+    loadComponent: () =>
+      import('./modules/lanchonetes/lanchonetes.component').then((m) => m.LanchonetesComponent)
+  },
+  {
+    path: 'lanchonete/:nome',
+    loadComponent: () =>
+      import('./modules/lanchonetes-info/lanchonetes-info.component').then((m) => m.LanchonetesInfoComponent)
+  },
+  {
+    path: 'comparar-precos',
+    loadComponent: () =>
+      import('./modules/comparar-precos/comparar-precos.component').then((m) => m.CompararPrecosComponent)
+  },
+  {
+    path: 'contato',
+    loadComponent: () =>
+      import('./modules/contato/contato.component').then((m) => m.ContatoComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./modules/error/pages/error-404/error-404.component').then((m) => m.Error404Component)
+  }
 ];
